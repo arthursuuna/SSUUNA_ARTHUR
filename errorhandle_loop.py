07f48e185e884a3cdc193853ab838f53bcdec9a0
arthursuuna <ssuunaarthur@gmail.com>
2025-05-26 01:09:47 +0300

    fixed error

--- errorhandle_loop.py
+++ errorhandle_loop.py
@@ -2,11 +2,12 @@ while True:
     try:
         num1 = float(input("Enter the first number: "))
         num2 = float(input("Enter the second number: "))
+        result = num1 / num2
     except ValueError:
         print("Invalid input. Please enter numeric values.")
-        result = num1 / num2
     except ZeroDivisionError:
         print("Division by zero is not allowed.")
     else:
         print("Result of division is:", result)
         break
+
