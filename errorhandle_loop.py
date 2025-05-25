6db005e8e4247127d233f310497b5f70ff82984a
arthursuuna <ssuunaarthur@gmail.com>
2025-05-26 00:51:31 +0300

    Added loop to repeated ask user for two numbers

---
+++ errorhandle_loop.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
