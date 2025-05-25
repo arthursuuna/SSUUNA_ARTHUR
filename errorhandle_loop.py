342f12e97acd6396716222470a34d1f3013035c3
arthursuuna <ssuunaarthur@gmail.com>
2025-05-26 01:01:25 +0300

    Added error handling for valueError and ZaroDivisionError

--- errorhandle_loop.py
+++ errorhandle_loop.py
@@ -4,3 +4,6 @@ while True:
         num2 = float(input("Enter the second number: "))
     except ValueError:
         print("Invalid input. Please enter numeric values.")
+        result = num1 / num2
+    except ZeroDivisionError:
+        print("Division by zero is not allowed.")
